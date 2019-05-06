bd402454704999ef438faa9f1d22521a03a51046
Connie <connie@dhcp-10-250-74-210.harvard.edu>
2019-05-06 10:29:13 -0400

    added github icon, added browser tab icon

--- parts/htmlhead.php
+++ parts/htmlhead.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta name="description" content="Portfolio website for Constance Bainbridge: musician, multi-media artist, programmer, psychology researcher."/>
 <meta name="keywords" content="constance,bainbridge,may,mei,ohara,violin,music lab,harvard,psychology,research,brainbridge,">
+<link rel="icon" href="img/logo.png">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.4/css/swipebox.min.css">
 <link href="https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei|Audiowide" rel="stylesheet">
--- parts/social.php
+++ parts/social.php
@@ -1,2 +1,3 @@
+<a href="https://github.com/conbainbridge" target="_blank"><img src="img/github.svg" class="icon" style="min-width: 30px" alt="github icon" /></a><br/>
 <a href="https://www.linkedin.com/in/constanceb/" target="_blank"><img src="img/linkedin.png" class="icon" style="min-width: 30px" alt="linkedin icon" /></a><br/>
 <a href="https://twitter.com/conBainbridge" target="_blank"><img src="img/twitter.png" class="icon" style="min-width: 30px" alt="twitter icon" /></a>
